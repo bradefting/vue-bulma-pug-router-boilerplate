d4736ccd4afb470e66aec89caf4b13e23c5a60d4
Brad Efting <bradefting@Brad-Eftings-MacBook-Pro-11.local>
2020-08-04 23:16:21 -0600

    add linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,21 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': 0,
+    'no-use-before-define': ['error', { 'functions': false }],
+    'import/extensions': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': ['error', {'props': true, 'ignorePropertyModificationsFor': ['state', 'Vue'] }],
+    'arrow-parens': [2, 'as-needed', { 'requireForBlockBody': true }],
+    'import/order': ['error', {'groups': ['index', 'external', 'sibling', 'parent', 'internal', 'builtin']}],
+    'operator-linebreak': 'off',
+    'no-else-return': [2, { 'allowElseIf': true }],
+    'object-curly-newline': 'off',
+    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 1 }],
+    'operator-assignment': 'off',
+    'no-alert': 'off'
   },
 };
